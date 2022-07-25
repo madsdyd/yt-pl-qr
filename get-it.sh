d7a4ecc9b277d5fa05c9ece3fd0229ea016acd47
Mads Bondo Dydensborg <mads@dydensborg.dk>
2022-07-25 11:56:25 +0200

    Updated to work with current playlists, etc

--- get-it.sh
+++ get-it.sh
@@ -13,6 +13,44 @@ function die() {
     exit 1
 }
 
+# Lets color a bit. This is clearly a waste of time... (setup in load function).
+OUTPUTCOLOR=
+NOCOLOR=
+SCRIPTNAME="yr-to-qr-thumb"
+function die() {
+    echo "${OUTPUTCOLOR}[${SCRIPTNAME}]${NOCOLOR} $(date +%T) ERROR:" "$@"
+    # echo "$(date +%T.%N) ERROR:" "$@" >> ${LOG_FILE}
+    exit 1
+}
+
+function info() {
+    echo "${OUTPUTCOLOR}[${SCRIPTNAME}]${NOCOLOR} $(date +%T) INFO:" "$@"
+    # echo "$(date +%T.%N) INFO:" "$@" >> ${LOG_FILE}
+}
+
+function warn() {
+    echo "${OUTPUTCOLOR}[${SCRIPTNAME}]${NOCOLOR} $(date +%T) WARN:" "$@"
+}
+
+function debug() {
+    echo "${OUTPUTCOLOR}[${SCRIPTNAME}]${NOCOLOR} $(date +%T) DEBUG:" "$@"
+}
+
+# Setup some stuff.
+
+# Set some variables on load - most "important": If tty output, lets put some colors on.
+function onLoad() {
+  if [ -t 1 ] ; then
+    OUTPUTCOLOR=$(tput setaf 2)  # Green
+    NOCOLOR=$(tput sgr0)
+  fi
+}
+onLoad
+
+
+
+
+
 ######################################################################
 function usage() {
     echo "Usage: $0 <playlistid> <apikey>"
@@ -41,7 +79,7 @@ BACKGROUND="${BASEDIR}/templates/background.png"
 # Approach
 # 1: Get the list of videos in json format, store to tmp file
 ALLINFO=$(mktemp)
-echo "Getting content of playlist"
+info "Getting content of playlist"
 # Note the 50 limitation. - more than that requires pagination.
 curl -s -o "$ALLINFO" 'https://www.googleapis.com/youtube/v3/playlistItems?maxResults=50&part=snippet%2C+id&playlistId='$PLAYLISTID'&key='$APIKEY || die "Unable to get playlist info from youtube, using PLAYLISTID=$PLAYLISTID and APIKEY=$APIKEY"
 
@@ -61,11 +99,13 @@ curl -s -o "$ALLINFO" 'https://www.googleapis.com/youtube/v3/playlistItems?maxRe
 # }
 # Not checking for it though.
 
+debug "Output from YT:"
+cat ${ALLINFO}
 
-echo "Parsing response in $ALLINFO"
+info "Parsing response in $ALLINFO"
 # 2: "Parse" all the videos for title and videoId
 LENGTH=$(jq '.items | length' $ALLINFO) || die "Unable to get length of items in response"
-echo "Number of items in response: $LENGTH"
+info "Number of items in response: $LENGTH"
 test "$LENGTH" -eq 0 && {
     cat $ALLINFO
     die "Unable to parse $ALLINFO (see above) or no elements in playlist"
@@ -78,38 +118,65 @@ do
     TITLE=$(jq '.items['$i'].snippet.title' $ALLINFO) || die "Unable to get title for item $i in response. See $ALLINFO"
     KIND=$(jq '.items['$i'].snippet.resourceId.kind' $ALLINFO) || die "Unable to get kind for item $i in response. See $ALLINFO"
     VIDEOID=$(jq '.items['$i'].snippet.resourceId.videoId' $ALLINFO) || die "Unable to get videoId for item $i in response. See $ALLINFO"
-
+    THUMBNAIL=$(jq '.items['$i'].snippet.thumbnails.maxres.url' $ALLINFO) || die "Unable to get thumbnail for item $i in response. See $ALLINFO"
+    
     # Check we have the right kind, and a title is present
     test -n "$TITLE" || die "Title not set for item $i"
     test "$KIND" = "\"youtube#video\"" || die "Item $i does not have kind youtube#video, but $KIND"
 
     # Status information
-    echo "Item $i has id $VIDEOID and title $TITLE"
+    info "Item $i has id $VIDEOID and title $TITLE"
 
-    TITLE=$(echo $TITLE | sed 's/^\"//;s/\"$//;s/\s/_/g')
+    # Clean up names for ", to make easier to use filenames
+    TITLE=$(echo $TITLE | sed 's/[\(\)\&\!\:\"\$\?\#\/]//g;s/\s/_/g' | sed "s/'//g")
+    # These needs " front and back removed
     VIDEOID=$(echo $VIDEOID | sed 's/^\"//;s/\"$//')
+    THUMBNAIL=$(echo ${THUMBNAIL} | sed 's/^\"//;s/\"$//')
+
+    debug "Videoid sanitized to ${VIDEOID}"
+    debug "Title sanitized to ${TITLE}"
     
     # 3: Use curl to retrieve the thumbnail in high resolution
-    echo "Getting thumbnail in high res"
+    info "Getting thumbnail in high res from ${THUMBNAIL}"
     THUMB=$(mktemp --suffix=.jpg)
-    # Maxresdefault does not seem to work for all, so use hqdefault. This is 480x360 in all my tests...
-    curl -s -o "$THUMB" 'http://img.youtube.com/vi/'$VIDEOID'/hqdefault.jpg' || die "Unable to get thumb for item $i, videoId=$VIDEOID"
+    debug "Storing thumb in ${THUMB}"
+    curl -s -o "$THUMB" "${THUMBNAIL}" || die "Unable to get thumb for item $i, videoId=$VIDEOID"
+    debug "Dimensions of thumb:" $(identify -format '%wx%h' "${THUMB}")
 
+    # TESTING: Resize thumb to 480x360
+    info "Resizing thumb"
+    THUMB2=$(mktemp --suffix=.jpg)
+    convert "${THUMB}" -resize 480x360 "${THUMB2}"
+    THUMB=${THUMB2}
+    debug "Dimensions of thumb after resize:" $(identify -format '%wx%h' "${THUMB}")
+    
+    
     # 4: Use qrencode to create the qrcode
     QR=$(mktemp --suffix=.png)
     qrencode -o "$QR" -t png --size=6 'https://www.youtube.com/watch?v='$VIDEOID || die "Error calling qrencode for item $i, videoId=$VIDEOID"
+    debug "Dimensions of qr:" $(identify -format '%wx%h' "${QR}")
     # 5: (Scale the image) and overlay qrencode on it, together with a frame...
     TMPIMG1=$(mktemp --suffix=.png)
     # First the film frame and thumb.
+    info "Compositing thumb onto film frame"
+    debug "Dimensions of frame:" $(identify -format '%wx%h' "${FRAME}")
     composite -gravity center "$THUMB" "${FRAME}" "$TMPIMG1" || die "Unable to composite film frame on thumb for item $i, videoId=$VIDEOID"
+    debug "Dimensions of ${TMPIMG1}:" $(identify -format '%wx%h' "${TMPIMG1}")
     # Then, put it on the background
+    info "Adding background"
+    debug "Dimensions of background:" $(identify -format '%wx%h' "${BACKGROUND}") 
     TMPIMG2=$(mktemp --suffix=.png)
-    convert "$BACKGROUND" "$TMPIMG1" -geometry +96+0 -composite "$TMPIMG2"
+    convert "$BACKGROUND" "$TMPIMG1" -geometry +96+0 -composite "$TMPIMG2" || die "Unable to convert file ${TMPIMG1}"
+    debug "Dimensions of tmpimg2:" $(identify -format '%wx%h' "${TMPIMG2}")
     # And, put the qr on top, then into the final picture
     # 6: Save the final image, using the title of the video as the name
+    info "Putting QR code on top"
     OUTPUT="$i-$TITLE.png"
-    convert "$TMPIMG2" "$QR" -geometry +0+97 -composite "$OUTPUT"
-    echo "Final image in $OUTPUT"
+    convert "$TMPIMG2" "$QR" -geometry +0+97 -composite "$OUTPUT" || die "Unable to convert file ${TMPIMG2} to ${OUTPUT}"
+    info "Final image in $OUTPUT"
+    debug "Dimensions of final image:" $(identify -format '%wx%h' "${OUTPUT}")
+
+    exit 1
     
 done
 
